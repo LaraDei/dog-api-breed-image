f1afa9bd894979ddbd180c228f8dda13b345a0e4
Lara Dei <59483736+LaraDei@users.noreply.github.com>
2020-09-22 12:30:02 -0700

    initial upload

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function getDogImage() {
+   var selectedDog = $("#breed").val();
+   let dogURL = selectedDog.replace(/-/i,'/').toLowerCase();
+   fetch(`https://dog.ceo/api/breed/${dogURL}/images/random`)
+     .then(response => response.json())
+     .then(responseJson => 
+       displayResults(responseJson))
+     .catch(error => alert('Something went wrong. Try a different breed.'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  if( responseJson.error){
+    alert('Something went wrong. Try a different breed.')
+  }
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
+
+
+
